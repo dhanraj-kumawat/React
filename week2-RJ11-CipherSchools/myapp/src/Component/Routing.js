e518dc6e61a912840b0c4c0e7a74f44043b3863a
Dhanraj-Kumawat <kumawatdhanraj8@gmail.com>
2023-02-04 13:42:12 +0530

    added

---
+++ week2-RJ11-CipherSchools/myapp/src/Component/Routing.js
@@ -0,0 +1,12 @@
+import react from react;
+
+function Routing() {
+    return (
+      <div className="">
+        <h1>About us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, quibusdam! Dolor quia sint aliquam, laborum optio debitis, dicta vero porro dignissimos eius tenetur eveniet! Ducimus enim tenetur molestiae? Deleniti laboriosam tempore, eveniet fugit nihil dicta voluptate error sapiente quia vel.</p>
+      </div>
+    );
+  }
+  
+  export default Routing;
